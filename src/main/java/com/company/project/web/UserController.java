7f601494bc8a6d4ce1fb9e5280f10f6ad405c93b
yuan <lingdianguole@163.com>
2018-08-30 17:27:10 +0800

    ent

--- src/main/java/com/company/project/web/UserController.java
+++ src/main/java/com/company/project/web/UserController.java
@@ -1,21 +1,22 @@
 package com.company.project.web;
+
 import com.company.project.core.Result;
 import com.company.project.core.ResultGenerator;
 import com.company.project.model.User;
 import com.company.project.service.UserService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.ibatis.jdbc.SQL;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.web.bind.annotation.*;
+import tk.mybatis.mapper.entity.Condition;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 /**
-* Created by CodeGenerator on 2018/08/30.
-*/
+ * Created by CodeGenerator on 2018/08/30.
+ */
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -28,10 +29,28 @@ public class UserController {
         return ResultGenerator.genSuccessResult();
     }
 
+    @PostMapping("/addAll")
+    public Result addAll(List<User> users) {
+        userService.save(users);
+        return ResultGenerator.genSuccessResult();
+    }
+
     @PostMapping("/delete")
     public Result delete(@RequestParam Integer id) {
         userService.deleteById(id);
         return ResultGenerator.genSuccessResult();
+    }
+
+    @PostMapping("/deletManey")
+    public Result deleteManey(@RequestParam String ids) {
+        userService.deleteByIds(ids); //形如1,2,3
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @GetMapping("/info")  //根据名字来查数据
+    public Result info(String nickName) {
+        User user = userService.findBy("nickName", nickName);
+        return ResultGenerator.genSuccessResult(user);
     }
 
     @PostMapping("/update")
@@ -40,10 +59,22 @@ public class UserController {
         return ResultGenerator.genSuccessResult();
     }
 
-    @PostMapping("/detail")
+    @GetMapping("/detail")
     public Result detail(@RequestParam Integer id) {
         User user = userService.findById(id);
         return ResultGenerator.genSuccessResult(user);
+    }
+
+    @GetMapping("/detailmany")
+    public Result detail(@RequestParam String ids) {
+        List<User> users = userService.findByIds(ids);
+        return ResultGenerator.genSuccessResult(users);
+    }
+
+    @GetMapping("/infocondition")
+    public Result info(@RequestParam Condition condition) {
+        List<User> users = userService.findByCondition(condition);
+        return ResultGenerator.genSuccessResult(users);
     }
 
     @PostMapping("/list")
